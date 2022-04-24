4a89b40fcbba5fc0bc3e78fe579ddf85395b4556
Thejus <git@tigerraw22.anonaddy.com>
2022-04-24 13:04:20 +0530

    changed  to ""

--- task-1/team-16/script1.sh
+++ task-1/team-16/script1.sh
@@ -4,9 +4,9 @@ function request_ip() {
     while read -r line; do
         ip=$(echo "$line" | awk '{print $1}')
         date_Year=$(echo "$line" | awk '{print $4}')
-        echo 'On ' "${date_Year:1:11}"', a login attempt was made, with the request coming from the IP ' $ip 
-        
-    done < $LOG_FILE
+        echo 'On ' "${date_Year:1:11}"', a login attempt was made, with the request coming from the IP ' "$ip"
+
+    done <$LOG_FILE
 }
 request_ip
 #this sample script prints the IPs that have tried to access the server

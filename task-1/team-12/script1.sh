5016bf236a0b57205eb787afa8910e250e4c91e2
Glitchyi <advaitnarayan8@gmail.com>
2022-04-24 12:20:08 +0530

    Final changes and readability improvements

--- task-1/team-12/script1.sh
+++ task-1/team-12/script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#95.52.125.163 - - [19/Dec/2020:20:51:02 +0100] "GET /images/stories/raith/almenland_logo.jpg HTTP/1.1" 200 21490 "http://www.almhuette-raith.at/" "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.195 Safari/537.36" "-"
-#$1
+# Using AWK find the IP Adresses and dates from a line in the log file and then formating and displaying the output using echo.
+
 while read l; do
     ipadd=$(echo $l | awk '{print $1}')
     date=$(echo $l | awk '{print substr($4,2,length($4)-10)}')
--- task-1/team-12/script3.sh
+++ task-1/team-12/script3.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+# Using grep command to find all occurrences of the given IP address and then use AWK to filter and format out the output.
+
 LOGFILE=$1
 ipadd=$2
-echo "Requests From "$2
+echo "Requests From "$2;
+echo "--------------"
 grep $2 $1 | awk {'print substr($6,2) " " substr($7,2) " " substr($8,1,length($8)-1) " " substr($12,2) " " $13 " " $14 " " substr($15,1,length($15)-1) " on " substr($4,2,length($4)-10)'} | sort | uniq -c | sort -nr
-

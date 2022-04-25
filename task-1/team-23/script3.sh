24032723a013aa7075bff68d060dd5aa668dbc60
sanumuhammedc <sanumuhammed.c@gmail.com>
2022-04-26 00:08:15 +0530

    Fixed an error in script 3

--- task-1/team-23/script3.sh
+++ task-1/team-23/script3.sh
@@ -11,12 +11,7 @@ read -p "Enter IP address to see requests: " target
 function request_by_ip( ) {
     while read line; do
         ip=$(echo $line | awk '{print $1}')
-
-        if [ $ip == $target ];
-        then
-            echo $line | awk -F "]" '{print $2}'
-        fi
-
+        echo $line | grep $target | awk -F "]" '{print "Result: "$2}'
     done < $LOG_FILE
 }
 request_by_ip

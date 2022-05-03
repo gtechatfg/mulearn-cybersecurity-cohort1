3ad8ca5c2d83770c5cd3bae8468a9e954bfdfa30
m-s-13 <102453929+m-s-13@users.noreply.github.com>
2022-05-03 14:46:31 +0530

    Create script1.sh

---
+++ task-1/team-24/script1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+LOG_FILE="/..access.log"
+function request_ip() {
+    while read -r line; do
+        ip=$(echo "$line" | awk '{print $1}')
+        date_Year=$(echo "$line" | awk '{print $4}')
+        echo 'On' "${date_Year:1:11}"',a login attempt was made, with the request coming from the IP ' "$ip"
+    done < $LOG_FILE
+}
+request_ip
+#this sample script prints the IPs that have tried to access the server

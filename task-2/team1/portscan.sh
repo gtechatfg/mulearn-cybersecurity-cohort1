4a15798deb6dff78a585f6c5606ed388847d430d
Anandhu-V-S <hackerssultan113@gmail.com>
2022-05-25 18:04:02 +0530

    Completed 4 task

---
+++ task-2/team1/portscan.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Enter the ip address of machine:"
+read ip
+hostname=$ip
+for port in {1..65535};do
+	2>/dev/null echo > /dev/tcp/$hostname/$port
+	 if [ $? == 0 ]
+	then
+		{
+			echo "$port of the host $ip is open"
+		}
+	fi
+done
+
+
+
+

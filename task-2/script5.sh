46232158dc15b819b959bae196e6065f34b01593
Hemant Krishnan <96692095+hemantkrishnan4@users.noreply.github.com>
2022-06-03 17:17:22 +0530

    Update script5.sh

--- task-2/script5.sh
+++ task-2/script5.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-curl -X GET https://reqbin.com/echo/get/json -H "Accept: application/json" -H "Authorization: Bearer "mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y"" 
+
+echo "User vinay : "
+curl -X GET http://gincy.pythonanywhere.com/getid -H "Accept: */*" -H "Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidmluYXkiLCJpZCI6NCwiZXhwIjoxNzAzOTQ5NDc5fQ.-7GoR85YvyoZbfhP5PjdX3RyeMrpmvCa8N0i9bXqvm8""
+
+echo "---------------------------------------------------------------------------"
+
+echo "User johan : "
+curl -X GET http://gincy.pythonanywhere.com/getid -H "Accept: */*" -H "Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiam9oYW4iLCJpZCI6NCwiZXhwIjoxNzAzOTQ5NDc5fQ.UFV_KQu1UGReoBzZ9QiJIcTa6XfZm1OnXoypLlb8icg""
+
+echo "---------------------------------------------------------------------------"
+
+echo "User alice : "
+curl -X GET http://gincy.pythonanywhere.com/getid -H "Accept: */*" -H "Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWxpY2UiLCJpZCI6NCwiZXhwIjoxNzAzOTQ5NDc5fQ.av8RVoE3oZhK70238Gd43EzOYHsldnYTiyupJpSUlVU""
